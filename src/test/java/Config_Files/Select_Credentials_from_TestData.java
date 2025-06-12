b90d51acc353a6a180d7a2fa7912dbbd08c68fdb
dthirum4 <devibala.thirumalraj@optum.com>
2025-06-12 11:30:01 +0530

    Update Select_Credentials_from_TestData.java

--- src/test/java/Config_Files/Select_Credentials_from_TestData.java
+++ src/test/java/Config_Files/Select_Credentials_from_TestData.java
@@ -10,8 +10,9 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class Select_Credentials_from_TestData {
 	
 	public static String get_snowflake_credentials(int row, int column) throws IOException, Exception {
-
-		FileInputStream fis = new FileInputStream(".\\TestData\\Snowflake_Credentials.xlsx");
+		String Snowflake_cred_path = new File("TestData/Snowflake_Credentials.xlsx").getAbsolutePath();
+		FileInputStream fis = new FileInputStream(Snowflake_cred_path);
+		//FileInputStream fis = new FileInputStream(".\\TestData\\Snowflake_Credentials.xlsx");
 		XSSFWorkbook workbook = new XSSFWorkbook(fis);
 		XSSFSheet sheet = workbook.getSheetAt(0);
 		return sheet.getRow(row).getCell(column).getStringCellValue();
@@ -22,16 +23,17 @@ public class Select_Credentials_from_TestData {
 		//String path=Paths.get("TestData","Config_Non_User_Credentials.xlsx").toAbsolutePath().toString();
 		//FileInputStream fis = new FileInputStream(".\\TestData\\Config_Non_User_Credentials.xlsx");
 		//FileInputStream fis = new FileInputStream(path);
-		String path = new File("TestData/Config_Non_User_Credentials.xlsx").getAbsolutePath();
-		FileInputStream fis = new FileInputStream(path);
+		String Non_user_cred_path = new File("TestData/Config_Non_User_Credentials.xlsx").getAbsolutePath();
+		FileInputStream fis = new FileInputStream(Non_user_cred_path);
 		XSSFWorkbook workbook = new XSSFWorkbook(fis);
 		XSSFSheet sheet = workbook.getSheetAt(0);
 		return sheet.getRow(row).getCell(column).getStringCellValue();
 	}
 
 	public static String get_dashboard_list(int row, int column) throws IOException, Exception {
-
-		FileInputStream fis = new FileInputStream(".\\TestData\\Dashboard_List.xlsx");
+		String dashboard_list_path = new File("TestData/Dashboard_List.xlsx").getAbsolutePath();
+		FileInputStream fis = new FileInputStream(dashboard_list_path);
+		//FileInputStream fis = new FileInputStream(".\\TestData\\Dashboard_List.xlsx");
 		XSSFWorkbook workbook = new XSSFWorkbook(fis);
 		XSSFSheet sheet = workbook.getSheetAt(0);
 		return sheet.getRow(row).getCell(column).getStringCellValue();
@@ -39,8 +41,9 @@ public class Select_Credentials_from_TestData {
 	
 	public static int get_total_rows() throws IOException, Exception {
 
-		
-		FileInputStream fis = new FileInputStream(".\\TestData\\Dashboard_List.xlsx");
+		String dashboard_list_path = new File("TestData/Dashboard_List.xlsx").getAbsolutePath();
+		FileInputStream fis = new FileInputStream(dashboard_list_path);
+		//FileInputStream fis = new FileInputStream(".\\TestData\\Dashboard_List.xlsx");
 		try {
 		XSSFWorkbook workbook = new XSSFWorkbook(fis);
 		XSSFSheet sheet = workbook.getSheetAt(0);
@@ -53,8 +56,9 @@ public class Select_Credentials_from_TestData {
 	
 	public static String get_email_alert_credentials(int row, int column)
 			throws IOException, Exception {
-
-		FileInputStream fis = new FileInputStream(".\\TestData\\Email_Alert_Credentials.xlsx");
+		String Email_alert_cred_path = new File("TestData/Email_Alert_Credentials.xlsx").getAbsolutePath();
+		FileInputStream fis = new FileInputStream(Email_alert_cred_path);
+		//FileInputStream fis = new FileInputStream(".\\TestData\\Email_Alert_Credentials.xlsx");
 		XSSFWorkbook workbook = new XSSFWorkbook(fis);
 		XSSFSheet sheet = workbook.getSheetAt(0);
 		return sheet.getRow(row).getCell(column).getStringCellValue();
