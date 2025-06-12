555d0b1d0088d5cfad0def74454dcf9376e30e65
dthirum4 <devibala.thirumalraj@optum.com>
2025-06-12 11:18:18 +0530

    Update Select_Credentials_from_TestData.java

--- src/test/java/Config_Files/Select_Credentials_from_TestData.java
+++ src/test/java/Config_Files/Select_Credentials_from_TestData.java
@@ -2,6 +2,7 @@ package Config_Files;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.File;
 
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -18,8 +19,11 @@ public class Select_Credentials_from_TestData {
 	
 	public static String get_config_non_user_credentials(int row, int column)
 			throws IOException, Exception {
-
-		FileInputStream fis = new FileInputStream(".\\TestData\\Config_Non_User_Credentials.xlsx");
+		//String path=Paths.get("TestData","Config_Non_User_Credentials.xlsx").toAbsolutePath().toString();
+		//FileInputStream fis = new FileInputStream(".\\TestData\\Config_Non_User_Credentials.xlsx");
+		//FileInputStream fis = new FileInputStream(path);
+		String path = new File("TestData/Config_Non_User_Credentials.xlsx").getAbsolutePath();
+		FileInputStream fis = new FileInputStream(path);
 		XSSFWorkbook workbook = new XSSFWorkbook(fis);
 		XSSFSheet sheet = workbook.getSheetAt(0);
 		return sheet.getRow(row).getCell(column).getStringCellValue();
